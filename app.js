e1c46e8679967c682c9bb7d83c36f4990b65e968
Ljblack1 <152862088+Ljblack1@users.noreply.github.com>
2024-02-14 10:40:10 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 
 const preloder1 = document.querySelector('.preloader');
 const preloder2 = document.querySelector('.preloader2');
+const splahScreen = document.querySelector('.splash_screen');
 const options = document.querySelectorAll('.option-btn');
 
 const loader = document.querySelector('.small-load');
+
 
 window.addEventListener('DOMContentLoaded', () => {
   preloder1.style.display = "flex";
@@ -12,12 +14,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
 window.addEventListener('load', () => {
   setTimeout(() => {
-    preloder1.style.display = "none";
-
+    splahScreen.style.display = "none";
     setTimeout(() => {
-      preloder2.style.display = "none";
+      preloder1.style.display = "none";
+  
+      setTimeout(() => {
+        preloder2.style.display = "none";
+      }, 5000)
     }, 5000)
-  }, 5000)
+  }, 5000);
 })
 
 options.forEach((option) => {
