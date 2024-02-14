0be501fb6411c41f70dbd55ae7595262944804e7
Ljblack1 <152862088+Ljblack1@users.noreply.github.com>
2024-02-14 05:56:56 -0800

    Add files via upload

--- sportybet/aviator.js
+++ sportybet/aviator.js
@@ -99,9 +99,9 @@ function displayAlert(text, action) {
 
 loginContainer.style.display = "flex";
 
-window.addEventListener('DOMContentLoaded', () => {
-  move.style.display = "none"; 
-})
+// window.addEventListener('DOMContentLoaded', () => {
+//   move.style.display = "none"; 
+// })
 
 let value = 0;
 
@@ -120,26 +120,26 @@ btn.addEventListener("click", () => {
 
   value++;
 
-  if (value > moves.length -1) {
-    value = 0;
-  }
+  // if (value > moves.length -1) {
+  //   value = 0;
+  // }
   
-  loader.style.transform = `rotate(${1440}deg)`;
-  loader.style.transition = `all ${2}s ease`;
+  // loader.style.transform = `rotate(${1440}deg)`;
+  // loader.style.transition = `all ${2}s ease`;
   
   loadingFill.style.width = `${0}%`;
   
   setTimeout(() => {
     pending.style.display = "none";
     
-    loader.style.transform = `rotate(${0}deg)`;
-    loader.style.transition = `all ${0}s ease`;
+    // loader.style.transform = `rotate(${0}deg)`;
+    // loader.style.transition = `all ${0}s ease`;
     
-    move.style.display = "block"; 
+    // move.style.display = "block"; 
     
     loadingFill.style.width = `${100}%`;
 
-    move.textContent = `${moves[value]}`;
+    // move.textContent = `${moves[value]}`;
   }, 3000);
 
   // loadingFill.classList.add("hide-fill");
